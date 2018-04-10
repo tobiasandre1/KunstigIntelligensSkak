7bcd12921913d96097abfe8190ddd83091dae6a8
Tobias <hoejsgaard.tobias@gmail.com>
2018-04-10 15:00:57 +0200

    Added Class Files

---
+++ src/grp6/implementation/MoveChess.java
@@ -0,0 +1,16 @@
+package grp6.implementation;
+
+import grp6.interfaces.Move;
+import grp6.interfaces.Node;
+
+public class MoveChess implements Move {
+
+    @Override
+    public Node apply(Node state) {
+        if(state instanceof GameStateChess){
+            //TODO Implement here
+            return null;
+        }
+        return null;
+    }
+}
