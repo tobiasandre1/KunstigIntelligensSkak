01a7e084ca32d8b87339bb7404dfcf25400e600d
Tobias <hoejsgaard.tobias@gmail.com>
2018-04-10 16:08:08 +0200

    Added representation for board

--- src/grp6/implementation/GameStateChess.java
+++ src/grp6/implementation/GameStateChess.java
@@ -1,14 +1,16 @@
 package grp6.implementation;
 
+import grp6.interfaces.Move;
 import grp6.interfaces.Node;
 
 import java.util.List;
 
 public class GameStateChess implements Node {
 
+    char[] board = new char[65];
 
     @Override
-    public List<Node> getChildren() {
+    public List<Move> getMoves() {
         return null;
     }
 
